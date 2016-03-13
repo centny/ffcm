b57019fbca50354b47d379d51a985f22a2a76945
Centny <centny@gmail.com>
2016-03-13 23:01:32 +0800

    adding missing.

---
+++ io.vty.cswf.ffcm.test/FFCMTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace io.vty.cswf.ffcm.test
+{
+    [TestClass]
+    public class FFCMTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
